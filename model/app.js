d2147ed6d85625acd650e418162be6c3dc6fc688
a.Kuz <andrew.kuz@94gmail.com>
2017-12-19 16:39:05 +0200

    Alx: homework-2 0.30

--- model/app.js
+++ model/app.js
@@ -1,9 +1,7 @@
 "use strict";
 
 let myTv = new Tv('My TV', 'LG-3000', new Sound());
-
 let myFridge = new Fridge('My Fridge', 'NORD-200');
-
 let myLamp = new Lamp('My Lamp', 'Xiaomi Yeelight');
 
 let myDevices = [myTv, myFridge, myLamp];
