88324ab08df150a4703763dc442d07afcd3c84b3
a.Kuz <andrew.kuz@94gmail.com>
2017-12-19 13:58:44 +0200

    Alx: homework-2 0.25

--- model/app.js
+++ model/app.js
@@ -1,21 +1,17 @@
 "use strict";
 
 let myTv = new Tv('My TV 1', 'LG', new Sound());
-let myTvView = new View(myTv, document.getElementById("app"));
 
 let myTv1 = new Tv('My TV 2', 'LG-200', new Sound());
-let myTv1View = new View(myTv1, document.getElementById("app"));
 
 let myTv2 = new Tv('My TV 3', 'LG-3000', new Sound());
-let myTv2View = new View(myTv2, document.getElementById("app"));
 
-myTv1._sound.increase();
+myTv1._sound.increase(); // test
 
-let myDevices = [myTvView, myTv1View, myTv2View];
+let myDevices = [myTv, myTv1, myTv2];
 
 for(let i = 0; i < myDevices.length; i++){
-	myDevices[i].render();
+	new View(myDevices[i], document.getElementById("app")).render();
 }
-
 
 console.dir(myDevices);
